b7bbf04e2f14b58ee93105e13e4f4bb6c501cbb6
nllsdfx <nllsdfx@gmail.com>
2019-04-14 20:43:51 +0300

    VacancyGetFiltersQuery

--- src/main/java/ru/tjournal/api/sdk/action/Vacancies.java
+++ src/main/java/ru/tjournal/api/sdk/action/Vacancies.java
@@ -1,6 +1,7 @@
 package ru.tjournal.api.sdk.action;
 
 import ru.tjournal.api.sdk.client.ApiClient;
+import ru.tjournal.api.sdk.query.vacancies.VacancyGetFiltersQuery;
 import ru.tjournal.api.sdk.query.vacancies.VacancyGetQuery;
 import ru.tjournal.api.sdk.query.vacancies.VacancyLoadMoreQuery;
 
@@ -16,5 +17,9 @@ public class Vacancies extends Action {
 
     public VacancyLoadMoreQuery loadMore() {
         return new VacancyLoadMoreQuery(getClient());
+    }
+
+    public VacancyGetFiltersQuery getFilters() {
+        return new VacancyGetFiltersQuery(getClient());
     }
 }
---
+++ src/main/java/ru/tjournal/api/sdk/model/response/job/JobFilter.java
@@ -0,0 +1,9 @@
+package ru.tjournal.api.sdk.model.response.job;
+
+import lombok.Data;
+
+@Data
+public class JobFilter {
+    private String id;
+    private String title;
+}
