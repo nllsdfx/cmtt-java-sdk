fc622dae8cfbaa9a957482ae5cd9236ed5ed385d
nllsdfx <nllsdfx@gmail.com>
2019-04-15 14:29:34 +0300

    SubsiteTimelineQuery

--- src/main/java/ru/tjournal/api/sdk/action/Subsites.java
+++ src/main/java/ru/tjournal/api/sdk/action/Subsites.java
@@ -2,6 +2,7 @@ package ru.tjournal.api.sdk.action;
 
 import ru.tjournal.api.sdk.client.ApiClient;
 import ru.tjournal.api.sdk.query.subsite.SubsiteGetQuery;
+import ru.tjournal.api.sdk.query.subsite.SubsiteTimelineQuery;
 
 public class Subsites extends Action {
 
@@ -11,5 +12,9 @@ public class Subsites extends Action {
 
     public SubsiteGetQuery get() {
         return new SubsiteGetQuery(getClient());
+    }
+
+    public SubsiteTimelineQuery timeline() {
+        return new SubsiteTimelineQuery(getClient());
     }
 }
---
+++ src/test/java/ru/tjournal/api/sdk/query/subsite/SubsiteTimelineQueryTest.java
@@ -0,0 +1,31 @@
+package ru.tjournal.api.sdk.query.subsite;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import ru.tjournal.api.sdk.client.TJApiClient;
+import ru.tjournal.api.sdk.exception.ApiException;
+import ru.tjournal.api.sdk.exception.ClientException;
+import ru.tjournal.api.sdk.model.response.entry.Entry;
+import ru.tjournal.api.sdk.util.IntegrationTest;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SubsiteTimelineQueryTest {
+
+    @Test
+    @Category(IntegrationTest.class)
+    public void integration() throws ClientException, ApiException {
+        TJApiClient client = new TJApiClient("tst");
+        String subsiteId = client.entries().get().by("93603").execute().getSubsite().getId();
+        assertNotNull(subsiteId);
+        List<Entry> entries = client.subsites().timeline().id(subsiteId).sorting("new").count(5).execute();
+        assertNotNull(entries);
+        assertEquals(5, entries.size());
+        for (Entry entry : entries) {
+            assertEquals(subsiteId, entry.getSubsite().getId());
+        }
+    }
+
+}
